4b88d1b7e51cceb7a6fbdb908af8636fe621c8e7
Leonardo Larrad <leonardolarrad@gmail.com>
2023-02-02 18:15:27 -0400

    not-completed: push

--- academy/controllers/academy_controllers.py
+++ academy/controllers/academy_controllers.py
@@ -7,3 +7,9 @@ class Academy(http.Controller):
     @http.route('/academy/', auth='public', website=True)
     def index(self, **kw):
         return "Hello, world"
+
+    @http.route('/academy/courses', auth='public', website=True)
+    def courses(self, **kw):
+        return http.request.render('academy.course_website', {
+            'courses': http.request.env['academy.course'].search([])
+        })
