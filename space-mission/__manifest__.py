38f4911d305386e4a35c7582232b1f73e6d4b61f
Leonardo Larrad <leonardolarrad@gmail.com>
2023-02-03 16:32:35 -0400

    fix: update dependencies

--- space-mission/__manifest__.py
+++ space-mission/__manifest__.py
@@ -9,7 +9,7 @@
     'website': 'https://www.dimac21.com',
     'author': 'Soluciones DIMAC21',
     'license': 'LGPL-3',
-    'depends': ['base'],
+    'depends': ['base', 'mail'],
     'data': [        
         # security
         'security/space_mission_security.xml',
