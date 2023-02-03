dfe90d5e588f4d0f83e913bb8cb1ab8ae69625b8
Leonardo Larrad <leonardolarrad@gmail.com>
2023-02-03 15:09:01 -0400

    add: session_script

---
+++ scripts/session_script.py
@@ -0,0 +1,24 @@
+from xmlrpc import client
+
+url = 'https://leonardolarrad-odoo-technical-training-academy-7147007.dev.odoo.com'
+db = 'leonardolarrad-odoo-technical-training-academy-7147007'
+username = 'admin'
+password = 'admin'
+common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+
+# print the version
+print('client version:', common.version())
+
+# print user id
+uid = common.authenticate(db, username, password, {})
+print('user id:', uid)
+
+# call a method
+models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+
+model_access = models.execute_kw(
+    db, uid, password,
+    'academy.session', 'check_access_rights',
+    ['write'], {'raise_exception': False})
+
+print('academy.session access:', model_access)
