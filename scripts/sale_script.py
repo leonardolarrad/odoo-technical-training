a11f3200c4a835cf82e1e40ec075cee991f104e3
Leonardo Larrad <leonardolarrad@gmail.com>
2023-02-03 14:53:10 -0400

    update: `sale_script`

--- scripts/sale_script.py
+++ scripts/sale_script.py
@@ -7,4 +7,18 @@ password = 'admin'
 common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 
 # print the version
-print(common.version())
+print('client version:', common.version())
+
+# print user id
+uid = common.authenticate(db, username, password, {})
+print('user id:', uid)
+
+# call a method
+models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+
+model_access = models.execute_kw(
+    db, uid, password,
+    'sale.order', 'check_access_rights',
+    ['write'], {'raise_exception': False})
+
+print('sale.order access:', model_access)
