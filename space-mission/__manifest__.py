b11e5e06e6a399e20b9111e49c2fcfc63d04ebca
Leonardo Larrad <leonardolarrad@gmail.com>
2023-02-03 16:39:00 -0400

    fix: dependencies

--- space-mission/__manifest__.py
+++ space-mission/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Misión Espacial DIMAC21',
-    'summary': 'Misión Espacial de DIMAC21',
-    'description': 'Hasta la luna y de regreso.',
+    'name': 'DIMAC21 Space Mission',
+    'summary': 'An app to manage space missions',
+    'description': 'To the moon and back!',
     'version': '1.0',
     'category': 'DIMAC21',
     'website': 'https://www.dimac21.com',
-    'author': 'Soluciones DIMAC21',
+    'author': 'Soluciones DIMAC 21 C.A.',
     'license': 'LGPL-3',
-    'depends': ['base', 'mail'],
+    'depends': ['mail'],
     'data': [        
         # security
         'security/space_mission_security.xml',
